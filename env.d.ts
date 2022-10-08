395e7affcc81060e7f2eecaa3abb849f34ce39bb
sionnigjt <18980192376@163.com>
2022-10-08 15:24:20 +0800

    "初始化"

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+// declare module "*.vue" {
+//   import { DefineComponent } from "vue"
+//   const component: DefineComponent<{}, {}, any>
+//   export default component
+// }
+interface ImportMetaEnv {
+	readonly BASE_URL: string
+} 
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
